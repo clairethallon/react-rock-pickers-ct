4692aca510304fcc71f34307309198d7fb42b837
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-10-28 12:26:50 -0500

    comments added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import RockCounter from '../RockCounter/RockCounter';
 
 function App() {
-  let [totalCount, setTotalCount] = useState(0);
-  let [pickaxes, setPickaxes] = useState([]);
+  let [totalCount, setTotalCount] = useState(0); // set var for total Count of rocks
+  let [pickaxes, setPickaxes] = useState([]); // set var for pickaxes emoji array
 
-  let staticPickaxes = ['\u26CF', '\u26CF', '\u26CF'];
-
+  // create function to calculate rocks picked total to display on DOM
   const updateTotalCount = (increment) => {
-    setTotalCount(totalCount += increment);
-    displayRocks();
+    // set totalCount = (totalCount +/- increment) to get the total count of Rocks Picked
+    setTotalCount(totalCount += increment); 
+    displayRocks(); //update pickaxe emoji array
   }
 
       //   // in map:
@@ -22,22 +22,25 @@ function App() {
       // console.log( newArray );
 
 
+  // function to create array of pickaxe emojis to display on DOM
   const displayRocks = () => {
-    pickaxes = [];
+    pickaxes = []; // empty out emoji array
     //totalCount.map( ()=> { //todo - would like to get this working with map instead
+    //});
     for(let i=0; i<totalCount; i++){
       pickaxes = [...pickaxes, '\u26CF'];//<-pickaxe emoji
     }
-    //});
-    setPickaxes(pickaxes)
+  
+    setPickaxes(pickaxes) //set pickaxes array
   }
 
   return (
     <div>
       <header>
         <h1>React Rock Pickers</h1>
-
-        <h2>Total Count: {totalCount}</h2>
+        {/* display total rock count */}
+        <h2>Total Count: {totalCount}</h2> 
+        {/* display pickaxe emojis */}
         <p>{pickaxes}</p>
 
         <p>
@@ -46,18 +49,19 @@ function App() {
         </p>
       </header>
 
-      <h3>Luke</h3>
 
-      <RockCounter changeCountTACO={updateTotalCount} />
+      {/* send function updateTotalCount down to each RockCounter using props */}
+      <h3>Luke</h3>
+      <RockCounter changeCount={updateTotalCount} />
 
       <h3>JJ</h3>
-      <RockCounter changeCountTACO={updateTotalCount} />
+      <RockCounter changeCount={updateTotalCount} />
 
       <h3>Sam</h3>
-      <RockCounter changeCountTACO={updateTotalCount} />
+      <RockCounter changeCount={updateTotalCount} />
 
       <h3>Pete</h3>
-      <RockCounter changeCountTACO={updateTotalCount} />
+      <RockCounter changeCount={updateTotalCount} />
     </div>
   );
 }
--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -2,31 +2,44 @@ import React, { useState } from 'react';
 
 function RockCounter(props) {
 
-  const [isZero, setIsZero] = useState(true);
-  const [done, setDone] = useState(false);
+  const [isZero, setIsZero] = useState(true); //set bool for whether this picker's current count is zero
+  const [done, setDone] = useState(false); //set bool for whether this picker's current count is > 50
   let [count, setCount] = useState(0); //confused about let vs const here - only let works with setCount(count++)
 
   const increase = () => {
-    setCount(++count);
-    props.changeCountTACO(1);
-    if (count >= 50) {
-      setDone(true);
-    }
-    checkZero();
+    //function that increases this rock picker's count
+
+    setCount(++count);     //step 1: increase the count - basemode!
+    props.changeCount(1);     //step 2: send this increase (we're always adding 1) to parent
+    checkFifty();     //step 3: check if the count exceeds 50
+    checkZero();     //step 4: check if the count is zero
   }
 
   const decrease = () => {
-    if (count > 0) {
-      setCount(--count);
-      props.changeCountTACO(-1);
+    //function that decreases this rock picker's count
+
+    //step 0: IF the count is not already at zero (no negatives allowed)...
+    if (count > 0) {  
+      setCount(--count); //step 1: decrease the count
+      props.changeCount(-1); //step 2: send this decrease (always -1) to parent
+      checkFifty(); //step 3: check if the count exceeds 50
+      checkZero();  //step 4: check if the count is zero
     }
+  }
+
+  const checkFifty = () => {
+    //function that checks if the count is > 50 and toggles bool appropriately 
+
     if (count < 50) {
-      setDone(false);
+      setDone(false); 
+    } else {
+      setDone(true);
     }
-    checkZero();
   }
 
   const checkZero = () => {
+    //function that checks if the count is 0 and toggles bool appropriately 
+
     if (count === 0) {
       setIsZero(true);
     } else {
@@ -35,21 +48,26 @@ function RockCounter(props) {
   }
 
   const reset = () => {
-    props.changeCountTACO(-count);
-    setDone(false);
-    setCount(count = 0);
-    //setCount(0); why doesn't this work, but line above does? 
-    //console.log('count:', count);
-    checkZero();
+    //function that resets the count to zero
+
+    props.changeCount(-count); //in this case we want to send to parent what the count was BEFORE the reset, to subtract that amount from the total
+    setDone(false); //toggle bool
+    setCount(count = 0); //toggle count
+    //QUESTION! setCount(0); why doesn't this work, but line above does? 
+    //QUESTION! console.log('count:', count);
+    checkZero(); //check if count is zero (spoiler alert: it is)
   }
 
 
 
   return (
     <div>
+      {/* show count for this picker. If they're done (>50 rocks picked), show "DONE" */}
       <div>Rocks Picked: {count} {done ? <p>DONE</p> : <span></span>}</div>
       <div>
         <button onClick={increase}>Increase</button>
+
+        {/* display the 'decrease' button, unless the count is currently 0 (no negatives allowed)*/}
         {isZero ? <span></span> : <button onClick={decrease}>Decrease</button>}
         <button onClick={reset}>Reset</button>
 
